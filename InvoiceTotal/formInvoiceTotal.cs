1934785d8d808e8312d2d765c229bf64baee5a97
B33 <my@email.com>
2020-01-23 19:40:51 -0600

    Add comments

--- InvoiceTotal/formInvoiceTotal.cs
+++ InvoiceTotal/formInvoiceTotal.cs
@@ -19,8 +19,12 @@ namespace InvoiceTotal
 
     private void btnCalc_Click(object sender, EventArgs e)
     {
+      //gets contents of the subtotal box.
+        //Data validation is for suckers.
       double subtotal = Convert.ToDouble(txtSubtotal.Text);
       double discountPercent = 0;
+
+      //caculates what the discount percent should be 
       if(subtotal >= 100)
       {
         discountPercent = 0.2;
@@ -34,9 +38,11 @@ namespace InvoiceTotal
         discountPercent = 0.1;
       }
 
+      //caculates the invoice total
       double discountAmount = subtotal * discountPercent;
       double invoiceTotal = subtotal - discountAmount;
 
+      //assigns calculated values to text boxes.
       txtDiscountPercent.Text = discountPercent.ToString("p1");
       txtDiscountAmount.Text = discountAmount.ToString("c");
       txtTotal.Text = invoiceTotal.ToString("c");
